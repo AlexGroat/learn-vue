1fa2937faddb2015296ca37e785535d727fd8eca
Alex Groat <alex.groat.95@gmail.com>
2022-04-01 11:19:11 +1030

    practical component exercise #2 modal

--- main.js
+++ main.js
@@ -23,6 +23,28 @@ Vue.component("message", {
      `,
 });
 
+Vue.component("modal", {
+  props: [""],
+
+  template: `
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <div class="box">
+    // slot is user input
+        <slot></slot>
+    </div>
+  </div>
+//   Custom event is called on the button
+  <button class="modal-close is-large" aria-label="close" @click="$emit('close')">Open</button>
+</div>
+ `,
+});
+
 new Vue({
   el: "#root",
+
+  data: {
+      showModal: false
+  }
 });
