828233f73375bbbc05a9b33eb24387c89f00bd2b
Alex Groat <alex.groat.95@gmail.com>
2022-04-01 13:50:45 +1030

    component communication example #2 event dispatcher

--- main2.js
+++ main2.js
@@ -1,33 +1,78 @@
-/*!!!!!!!!!!!!!!! CHILD COMPONENT !!!!!!!!!!!!!!!!!!!!!*/
+// METHOD !
+// window.Event = new Vue();
+
+// /*!!!!!!!!!!!!!!! CHILD COMPONENT !!!!!!!!!!!!!!!!!!!!!*/
+// Vue.component("coupon", {
+//     props: [""],
+
+//     template: `
+//     /* blur: when an element has lost its focus, clicking off of
+//     the input after entering a value */
+//     <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+//    `,
+
+//    methods: {
+//        onCouponApplied() {
+//            // emitting an event, referrenced in index4.html line 19
+//           Event.$emit('applied');
+//        }
+//    }
+//   });
+
+// /*!!!!!!!!!!!!!!!!!!!!!! PARENT COMPONENT !!!!!!!!!!!!!!!!!!!!!*/
+// new Vue({
+//   el: "#root",
+
+//   data: {
+//       couponApplied: false
+//   },
+
+//   created() {
+//       Event.$on('applied', () => alert('Handle it'));
+//   }
+// });
+
+// METHOD 2
+
+window.Event = new (class {
+  constructor() {
+    this.vue = new Vue();
+  }
+
+  fire(event, data = null) {
+    this.vue.$emit(event, data);
+  }
+
+  listen(event, callback) {
+    this.vue.$on(event, callback);
+  }
+})();
+
 Vue.component("coupon", {
-    props: [""],
-  
-    template: `
+  props: [""],
+
+  template: `
     /* blur: when an element has lost its focus, clicking off of
     the input after entering a value */
     <input placeholder="Enter your coupon code" @blur="onCouponApplied">
    `,
 
-   methods: {
-       onCouponApplied() {
-           // emitting an event, referrenced in index4.html line 19
-          this.$emit('applied');
-       }
-   }
-  });
-
-/*!!!!!!!!!!!!!!!!!!!!!! PARENT COMPONENT !!!!!!!!!!!!!!!!!!!!!*/
-new Vue({
-  el: "#root",
-
-  data: {
-      couponApplied: false
-  },
-
   methods: {
     onCouponApplied() {
-      this.couponApplied = true;
+      // emitting an event, referrenced in index4.html line 19
+      Event.fire("applied");
     },
   },
 });
 
+new Vue({
+  el: "#root",
+
+  data: {
+    couponApplied: false,
+  },
+
+  created() {
+    Event.listen("applied", () => alert("Handle it"));
+  },
+});
